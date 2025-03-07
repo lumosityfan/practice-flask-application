05a73b1bda473efe2825669ec13ebd0debde372e
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-03-07 17:08:04 -0600

    Added error handling and mailing services and ability to edit profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import TextAreaField
+from wtforms.validators import Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -27,3 +29,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 from app.forms import RegistrationForm
+from app.forms import EditProfileForm
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
@@ -10,6 +11,13 @@ from urllib.parse import urlsplit
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from datetime import datetime, timezone
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -77,3 +85,18 @@ def user(username):
             {'author': user, 'age': 30, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
